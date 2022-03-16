3cdc74191aa0528d3474004cf90441f57658a7aa
Julia Pereira <juliapereira.odc@gmail.com>
2022-03-16 15:11:03 -0300

    implements conversor de moedas service

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,6 +1,6 @@
-import { Moeda } from './models/moeda.model';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
@@ -9,10 +9,11 @@ import { MoedaService, ConversorService } from './services';
 
 @NgModule({
   declarations: [
-    ConversorComponent
+    ConversorComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
   ],
   exports: [
     ConversorComponent
--- src/app/conversor/services/conversor.service.spec.ts
+++ src/app/conversor/services/conversor.service.spec.ts
@@ -11,6 +11,16 @@ describe('ConversorService', () => {
   });
 
   it('should be created', () => {
+    service.converter({
+      moedaDe: 'BRL',
+      moedaPara: 'USD',
+      valor: 20
+    }).subscribe({
+      complete: console.log,
+      next: console.log,
+      error: console.log
+    })
+    
     expect(service).toBeTruthy();
   });
 });
