2528d40181f3abb1a2c573e1f807ed57c4bb198d
Julia Pereira <juliapereira.odc@gmail.com>
2022-03-17 10:10:23 -0300

    implements conversão de moedas form

--- src/app/conversor/components/conversor.component.ts
+++ src/app/conversor/components/conversor.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Moeda, Conversao, ConversaoResponse } from '../models';
+import { MoedaService, ConversorService } from '../services';
 
 @Component({
   selector: 'app-conversor',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conversor.component.css']
 })
 export class ConversorComponent implements OnInit {
+  
+  moedas: Moeda[];
+  conversao: Conversao;
+  possuiErro: boolean;
+  conversaoResponse: ConversaoResponse;
 
-  constructor() { }
+  @ViewChild("conversaoForm", {static: true}) conversaoForm: NgForm;
+
+  constructor(
+    private moedaService: MoedaService,
+    private conversorService: ConversorService) { }
 
   ngOnInit(): void {
+    this.moedas = this.moedaService.listarTodas();
+    this.init();
+  }
+
+  /**
+   * Efetua a chamada para a conversao dos valores.
+   * @return void
+   */
+
+  init(): void{
+    this.conversao = new Conversao('USD', 'BRL', null);
+    this.possuiErro = false;
+  }
+
+  /**
+   * Efetua a chamada para a conversão dos valores.
+   * @return void
+   */
+
+  converter(): void{
+    if(this.conversaoForm.form.valid){
+      alert('Convertendo: ' + JSON.stringify(this.conversao));
+    }
   }
 
 }
